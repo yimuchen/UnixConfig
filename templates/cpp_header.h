7b7ce1b130e9179f1e89ea163eac10472dee6222
Enoch Chen <enochnotsocool@gmail.com>
2015-11-24 17:02:40 +0800

    Added c project templates

---
+++ templates/cpp_header.h
@@ -0,0 +1,13 @@
+#ifndef __PACKAGE_NAME_H__
+#define __PACKAGE_NAME_H__
+
+class PACKAGE_NAME {
+public:
+   PACKAGE_NAME();
+   virtual ~PACKAGE_NAME ();
+
+private:
+   /* data */
+};
+
+#endif // __PACKAGE_NAME_H__
