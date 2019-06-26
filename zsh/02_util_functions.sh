ca4226556d9baca7c29fbff2914c864a2a18afba
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2019-06-26 16:09:43 -0400

    Format change

--- zsh/02_util_functions.sh
+++ zsh/02_util_functions.sh
@@ -1,33 +1,32 @@
 #*******************************************************************************
- #
- #  Filename    : 02_util_function.sh
- #  Description : Common one line function call for easy modification
- #  Author      : Yi-Mu "Enoch" Chen [ ensc@hep1.phys.ntu.edu.tw ]
- #
+#
+#  Filename    : 02_util_function.sh
+#  Description : Common one line function call for easy modification
+#  Author      : Yi-Mu "Enoch" Chen [ ensc@hep1.phys.ntu.edu.tw ]
+#
 #*******************************************************************************
 
-function extract () {
-if [ -f $1 ] ; then
+function extract() {
+   if [ -f $1 ]; then
       case $1 in
-      *,tar.xz)    tar xvf $1     ;;
-      *.tar.bz2)   tar xvjf $1    ;;
-      *.tar.gz)    tar xvzf $1    ;;
-      *.bz2)       bunzip2 $1     ;;
-      *.rar)       unrar x $1     ;;
-      *.gz)        gunzip $1      ;;
-      *.tar)       tar xvf $1     ;;
-      *.tbz2)      tar xvjf $1    ;;
-      *.tgz)       tar xvzf $1    ;;
-      *.zip)       unzip $1       ;;
-      *.Z)         uncompress $1  ;;
-      *.7z)        7z x $1        ;;
-      *)           echo "don't know how to extract '$1'..." ;;
-   esac
-else
-   echo "'$1' is not a valid file!"
-fi
+      *,tar.xz) tar xvf $1 ;;
+      *.tar.bz2) tar xvjf $1 ;;
+      *.tar.gz) tar xvzf $1 ;;
+      *.bz2) bunzip2 $1 ;;
+      *.rar) unrar x $1 ;;
+      *.gz) gunzip $1 ;;
+      *.tar) tar xvf $1 ;;
+      *.tbz2) tar xvjf $1 ;;
+      *.tgz) tar xvzf $1 ;;
+      *.zip) unzip $1 ;;
+      *.Z) uncompress $1 ;;
+      *.7z) 7z x $1 ;;
+      *) echo "don't know how to extract '$1'..." ;;
+      esac
+   else
+      echo "'$1' is not a valid file!"
+   fi
 }
-
 
 function check_dir() {
    local check_path=""
@@ -35,31 +34,36 @@ function check_dir() {
 
    if [[ -f $check_path ]]; then
       print "${RED}ERROR!${NC} File $check_path exists instead of directory!\n"
-      return 1;
+      return 1
    fi
 
    if [[ ! -d $check_path ]]; then
       printf "${PURPLE}Warning:${NC} Directory doesn't exist, creating...\n"
       mkdir -p $check_path
       if [[ -d $check_path ]]; then
-         return true;
+         return true
       else
-         return false;
+         return false
       fi
    fi
 }
 
-function convert_pdf()
-{
+function listpath() {
+   for file in $@ ; do
+      echo $HOST:$(readlink -f $file)
+   done
+}
+
+function convert_pdf() {
    ## Options given at
    ## http://stackoverflow.com/questions/6605006/convert-pdf-to-image-with-high-resolution
    local input_file=$1
    local output_file=${input_file/.pdf/.png}
-   convert           \
-      -density 400   \
-      -trim          \
-      $input_file    \
-      -quality 100   \
+   convert \
+      -density 400 \
+      -trim \
+      $input_file \
+      -quality 100 \
       -sharpen 0x1.0 \
       $output_file
 }
