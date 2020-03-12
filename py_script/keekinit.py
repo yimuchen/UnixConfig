c39468c322e63d09eec7c39ec7f0be9e699c02da
Yi-Mu "Enoch" Chen <enochnotsocool@gmail.com>
2020-03-12 14:32:07 -0400

    Added scripts for running vscode deploy over non-conventional ssh settings

--- py_script/keekinit.py
+++ py_script/keekinit.py
@@ -3,39 +3,43 @@ import subprocess
 import argparse
 import argcomplete
 import getpass
+import sys
+import os
 from pykeepass import PyKeePass
 
 parser = argparse.ArgumentParser(
     'Starting a kerbose session with password stored in keepass database')
 
 parser.add_argument(
-    '--database','-d',
+    '--database',
+    '-d',
     type=str,
     default='/home/ensc/ArchConfig/AppConfig/Database.kdbx',
-    help='Keepass database file that we are going to search for entries'
-)
-
-parser.add_argument(
-    'domain',
-    type=str,
-    choices=['CERN', 'FNAL'],
-    help=
-    'Domain to connect to, currently only allows for CERN and FNAL identifications'
-)
+    help='Keepass database file that we are going to search for entries')
 
 argcomplete.autocomplete(parser)
 arg = parser.parse_args()
 
-kp = PyKeePass( arg.database,
-                password=getpass.getpass('Data base master password: ') )
-group = kp.find_groups(name='Internet',first=True)
-entry = kp.find_entries(title=arg.domain, first=True)
-lines = entry.notes.splitlines()
-kbsurl = next(line for line in lines if line.startswith('KERBOSE_URL') ).split()[1]
+kp = []
+try:
+    kp = PyKeePass(arg.database,
+               password=getpass.getpass('Data base master password: '))
+except:
+    print("Error opening database file")
+    sys.exit(1)
+group = kp.find_groups(name='Internet', first=True)
 
-## Running the Kinit command
-kinit = subprocess.run(
-    ['/usr/bin/kinit', '%s@%s' % ( entry.username, kbsurl )],
-    input=entry.password,
-    encoding='ascii'
-)
+for domain in ['CERN', 'FNAL']:
+  entry = kp.find_entries(title=domain, first=True)
+  lines = entry.notes.splitlines()
+  url = next(line for line in lines if line.startswith('KERBOSE_URL')).split()[1]
+
+  ## Running the Kinit command
+  kinit = subprocess.run([
+      '/usr/bin/kinit',
+      '{}@{}'.format(entry.username, url),
+      '-c',
+      '/tmp/kerberos_{}_{}'.format(os.getenv('USER'),domain)
+  ],
+                         input=entry.password,
+                         encoding='ascii')
