f9de402c156dbbf8c73d9a6303cbb4879a0a0c40
Yi-Mu Chen <enochnotsocool@gmail.com>
2022-02-28 10:57:15 +0100

    Update keevoms.py

--- py_script/keevoms.py
+++ py_script/keevoms.py
@@ -6,41 +6,43 @@ import getpass
 import sys
 import os
 import errno
+import configparser
 from pykeepass import PyKeePass
 
 parser = argparse.ArgumentParser(
-    """Initializing a voms proxy certificate on some remote server using the
-       credential stored in the keepass data base.""",
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  """Initializing a voms proxy certificate on some remote server using the
+  credential stored in the keepass database. Notice that the remote machine
+  connection settings will also be stored in the Keepass database to allow for
+  more complex settings.""",
+  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument(
-    '--database',
-    '-d',
-    default='{}/ArchConfig/AppConfig/Database.kdbx'.format(os.getenv('HOME')),
-    help='Keepass database file that we are going to search for entries')
+  '--database',
+  '-d',
+  default='{}/ArchConfig/AppConfig/Database.kdbx'.format(os.getenv('HOME')),
+  help='Keepass database file that we are going to search for entries')
 
 parser.add_argument(
-    '--certificate',
-    '-c',
-    type=str,
-    default='CERN GRID Certificate',
-    help='Title of the keepass database entry to uses for password query')
+  '--certificate',
+  '-c',
+  type=str,
+  default='CERN GRID Certificate',
+  help='Title of the keepass database entry to uses for password query')
 
 parser.add_argument(
-    '--servers',
-    '-s',
-    nargs='+',
-    default=['fnal-default'],
-    type=str,
-    help="""List of ssh servers to initialized voms proxy, notice that the
-    program expects to be able to run connect to the server via `ssh <server>`
-    without any user inputs. Configuration ~/.ssh/config accordingly.
-    """)
+  '--servers',
+  '-s',
+  nargs='+',
+  default=['FNAL', 'CERN', 'UMDCMS', 'CMSCONNECT'],
+  type=str,
+  help="""List of ssh servers to initialized voms proxy. These should be stored
+       in the notes part of the KeePass database, and the corresponding machine
+       should be able to log in without any additional input""")
 
 argcomplete.autocomplete(parser)
 args = parser.parse_args()
 
-## Openning the database
+## Opening the database
 kp = []
 try:
   kp = PyKeePass(args.database,
@@ -48,16 +50,33 @@ try:
 except:
   print("Error opening database file")
   sys.exit(1)
-# group = kp.find_groups(name='Internet', first=True)
+entry = kp.find_entries(title=args.certificate, first=True)
+server_settings = configparser.ConfigParser()
+server_settings.read_string(entry.notes)
 
+## Looping over
 for server in args.servers:
-  print(f"Running voms-proxy-init for {server}")
-  entry = kp.find_entries(title=args.certificate, first=True)
-  cmd = f'ssh {server} voms-proxy-init -voms cms --valid 192:00'
-  p = subprocess.Popen(cmd,
-                       shell=True,
-                       stdin=subprocess.PIPE,
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
-  p.communicate(input=str.encode(entry.password + '\n'))
-  p.wait()
+  try:
+    print(f"Running voms-proxy-init for {server}")
+    machine = server_settings[server]['machine']
+    krb5 = server_settings[server].get('KRB_FILE', '')
+    krb5 = krb5.replace('${UID}', str(os.getuid()))
+
+    ssh_cmd = ['ssh', machine]
+    voms_cmd = [
+      'voms-proxy-init',  # Main command
+      '-voms', 'cms',  # Virtual organization
+      '--valid', '192:00',  # Valid time
+      '--out', '\'${HOME}/x509up_u${UID}\''  # source file
+    ]
+    cmd = ' '.join(ssh_cmd + voms_cmd)
+    p = subprocess.Popen(cmd,
+                         shell=True,
+                         stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         env={'KRB5CCNAME': krb5})
+    p.communicate(input=str.encode(entry.password + '\n'))
+    p.wait()
+  except Exception as err:
+    print("Error in configuration:", err)
