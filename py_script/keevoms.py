55368ac2b01605a84b24601fc09132a7db18bc22
Yi-Mu Chen <enochnotsocool@gmail.com>
2022-03-24 09:32:48 +0100

    New format for keevoms initialization

--- py_script/keevoms.py
+++ py_script/keevoms.py
@@ -2,81 +2,59 @@
 import subprocess
 import argparse
 import argcomplete
-import getpass
-import sys
-import os
-import errno
-import configparser
-from pykeepass import PyKeePass
+import pykpb
 
 parser = argparse.ArgumentParser(
-  """Initializing a voms proxy certificate on some remote server using the
-  credential stored in the keepass database. Notice that the remote machine
-  connection settings will also be stored in the Keepass database to allow for
-  more complex settings.""",
+  """
+  Initializing a voms proxy certificate on some remote server over ssh command
+  passing using the credential stored in the keepass database.""",
   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument(
-  '--database',
-  '-d',
-  default='{}/ArchConfig/AppConfig/Database.kdbx'.format(os.getenv('HOME')),
-  help='Keepass database file that we are going to search for entries')
+parser.add_argument('--certificate',
+                    '-c',
+                    type=str,
+                    default='default_cert',
+                    help="""Dummy URL used to identify the certificate
+                         credentials. The URL should be prefix with cert:// in
+                         the database.""")
 
-parser.add_argument(
-  '--certificate',
-  '-c',
-  type=str,
-  default='CERN GRID Certificate',
-  help='Title of the keepass database entry to uses for password query')
-
-parser.add_argument(
-  '--servers',
-  '-s',
-  nargs='+',
-  default=['FNAL', 'CERN', 'UMDCMS', 'CMSCONNECT'],
-  type=str,
-  help="""List of ssh servers to initialized voms proxy. These should be stored
-       in the notes part of the KeePass database, and the corresponding machine
-       should be able to log in without any additional input""")
+parser.add_argument('--servers',
+                    '-s',
+                    nargs='+',
+                    default=[
+                      'fnal-default', 'lxplus7-bash',  #
+                      'umdcms-bash', 'cmsconnect-default'
+                    ],
+                    type=str,
+                    help="""List of ssh servers where you want to initialize
+                          your voms proxy. These should be the ssh servers that
+                          can be connect directly with the `ssh <server>`
+                          command without any additional input.""")
 
 argcomplete.autocomplete(parser)
 args = parser.parse_args()
 
-## Opening the database
-kp = []
-try:
-  kp = PyKeePass(args.database,
-                 password=getpass.getpass('Data base master password: '))
-except:
-  print("Error opening database file")
-  sys.exit(1)
-entry = kp.find_entries(title=args.certificate, first=True)
-server_settings = configparser.ConfigParser()
-server_settings.read_string(entry.notes)
+cred = pykpb.get_credentials(f'cert://{args.certificate}')
 
 ## Looping over
 for server in args.servers:
   try:
     print(f"Running voms-proxy-init for {server}")
-    machine = server_settings[server]['machine']
-    krb5 = server_settings[server].get('KRB_FILE', '')
-    krb5 = krb5.replace('${UID}', str(os.getuid()))
-
-    ssh_cmd = ['ssh', machine]
+    ssh_cmd = ['ssh', server]
     voms_cmd = [
       'voms-proxy-init',  # Main command
       '-voms', 'cms',  # Virtual organization
-      '--valid', '192:00',  # Valid time
-      '--out', '\'${HOME}/x509up_u${UID}\''  # source file
+      '--valid', '192:00',  # Valid time (8 days by default)
+      '--out',
+      '\'${HOME}/x509up_u${UID}\''  # proxy file in single quotes for env expansion
     ]
     cmd = ' '.join(ssh_cmd + voms_cmd)
     p = subprocess.Popen(cmd,
                          shell=True,
                          stdin=subprocess.PIPE,
                          stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE,
-                         env={'KRB5CCNAME': krb5})
-    p.communicate(input=str.encode(entry.password + '\n'))
+                         stderr=subprocess.PIPE)
+    p.communicate(input=str.encode(cred['password'] + '\n'))
     p.wait()
   except Exception as err:
     print("Error in configuration:", err)
