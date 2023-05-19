e2885a6daf879c9e185e014cbe885d5519a60646
Yi-Mu Chen <enochnotsocool@gmail.com>
2023-05-19 09:31:09 +0200

    Automatic detection of settings different machine types

--- zsh/00_machine_type.sh
+++ zsh/00_machine_type.sh
@@ -7,6 +7,28 @@
 #
 #-------------------------------------------------------------------------------
 
-#source ~/.custom-zsh/lxplus_rc.sh
-#source ~/.custom-zsh/ntugrid.sh
-source ~/.custom-zsh/personal_machine.sh
+case $HOST in
+ensc*)
+  source ~/.custom-zsh/personal_machine.sh
+  ;;
+cmslpc*)
+  source ~/.custom-zsh/cmssw.sh
+  ;;
+lxplus*)
+  source ~/.custom-zsh/cmssw.sh
+  ;;
+*umd.edu)
+  source ~/.custom-zsh/cmssw.sh
+  ;;
+login-el7.uscms.org)
+  source ~/.custom-zsh/cmssw.sh
+  ;;
+ndpc3)
+  source ~/.custom-zsh/ndpc3.sh
+  ;;
+*)
+  # Error message for debugging.
+  # echo "UNKNOWN MACHINE TYPE ${HOST}"
+  # Do nothing for unrecognized
+  ;;
+esac
