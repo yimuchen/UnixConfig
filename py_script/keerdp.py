6a063dba7e12ed7cf9129ecd3f1a19232ef88af7
Yi-Mu Chen <enochnotsocool@gmail.com>
2023-05-19 09:33:01 +0200

    Setting up for custom ports

--- py_script/keerdp.py
+++ py_script/keerdp.py
@@ -15,12 +15,13 @@ parser = argparse.ArgumentParser(
 parser.add_argument("--hostname",
                     "-v",
                     type=str,
-                    default='ndpc6',
+                    default='cerntscms.cern.ch',
                     help="""Where to log into the server. The server should also
                          have the user and password entries in the active
                          KeePassXC data base with the 'rdp://' prefix. The /u,
                          /v, and /p entires for the xfreexdp command will be
-                         automatically filled using the credentials found""")
+                         automatically filled using the credentials found.
+                         """)
 parser.add_argument("--args",
                     type=str,
                     default='/w:1920 /h:1080',
@@ -30,14 +31,23 @@ parser.add_argument('--dryrun',
                     action='store_true',
                     help="""If this flag is set, print the command on the screen
                       rather than running the command""")
+parser.add_argument('--port',
+                    type=int,
+                    default=3390,
+                    help="""The local port that is setup for tunneling. If this
+                         value is not None, than it expects the host to be
+                         reachable via /v:localhost""")
 
 args = parser.parse_args()
 argcomplete.autocomplete(parser)
 
 cred = pykpb.get_credentials(f'rdp://{args.hostname}')
+host_token = f'/v:{args.hostname}'
+if args.port:
+  host_token = f'/v:localhost:{args.port}'
 cmd = [
   'xfreerdp',  #
-  f'/v:{args.hostname}', f'/u:{cred["login"]}', f'/p:{cred["password"]}'
+  host_token, f'/u:{cred["login"]}', f'/p:{cred["password"]}'
 ]
 
 cmd += args.args.split()  # Splitting the additional arguments
